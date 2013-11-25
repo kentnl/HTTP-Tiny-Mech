5931c84327fefa62037ffee9ca582f190808ed7e
Kent Fredric <kentfredric@gmail.com>
2013-11-26 01:04:25 +1300

    Improve branch reset code to annihilate new tags

--- branch_reset.pl
+++ branch_reset.pl
@@ -13,4 +13,23 @@ if ( not env_true('TRAVIS') ) {
 diag("Resetting branch to \e[32m$ENV{TRAVIS_BRANCH}\e[0m @ \e[33m$ENV{TRAVIS_COMMIT}\e[0m");
 git( 'checkout', $ENV{TRAVIS_BRANCH} );
 git( 'reset', '--hard', $ENV{TRAVIS_COMMIT} );
+my $goodtag;
+do {
+  my $output = capture_stdout {
+    git( 'describe', '--tags', '--abbrev=0', $ENV{TRAVIS_BRANCH} );
+  };
+  ($goodtag) = split /\n/, $output;
+};
+diag("TIP Version tag is \e[32m$goodtag\e[0m");
+my @tags;
+do {
+  my $output = capture_stdout {
+    git('tag');
+  };
+  @tags = split /\n/, $output;
+};
+for my $tag (@tags) {
+  next if $tag eq $goodtag;
+  git( 'tag', '-d', $tag );
+}
 
