95420f3dea4e670afa40ea605df77e025138b1aa
Kent Fredric <kentfredric@gmail.com>
2017-03-11 02:29:00 +1300

    v1.001002
     - Bugfix: Avoid test failures with -Ddefault_inc_excludes_dot
     - Minor test refactoring
     - Added compat for older Test::More
    
     [Dependencies::Stats]
     - Dependencies changed since 1.001001, see misc/*.deps* for details
     - configure: +1 (recommends: ↑1)
     - develop: +14 ↑4 -3 (recommends: +1, suggests: ↑2)
     - test: +2 ↓1 (recommends: ↑1 ↓1)

--- lib/HTTP/Tiny/Mech.pm
+++ lib/HTTP/Tiny/Mech.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package HTTP::Tiny::Mech;
 
-our $VERSION = '1.001002';
+our $VERSION = '1.001003';
 
 # ABSTRACT: Wrap a WWW::Mechanize instance in an HTTP::Tiny compatible interface.
 
